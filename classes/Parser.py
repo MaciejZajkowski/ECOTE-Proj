52e5f6f14f9482f8bf2e0c1bdd5b2503df2457ea
Zajkos1 <mzx2000@gmail.com>
2023-05-30 13:52:30 +0200

    geting data finished

--- classes/Parser.py
+++ classes/Parser.py
@@ -2,11 +2,84 @@ import os
 
 class Parser():
     def __init__(self) -> None:
-        pass    
-    def _to_notation(lines:list):
-        for i,line in enumerate(lines):
-            print(line.)
         pass
+    
+    def _get_level(line):
+        return line.count('    ')
+    
+    def _look_for_assign(line:str):
+        if '=' in  line:
+            splits = line.split('=')
+            splits[0] = splits[0].replace(' ','')
+            return True,splits
+        else:
+            return False, 0
+    
+    def _split_by_operators(line):
+        operators = ['-','+','*','/']
+        for operator in operators:
+            line = line.replace(operator,'___TEMPORARY_SPLIT___')
+        line = line.replace(' ','')
+        return line.split('___TEMPORARY_SPLIT___')
+    
+    def _get_function(self,line):
+        if line[0:5] == 'class':
+            
+            if '(' in line and ')' in line:
+                parent = line[line.find("(")+1:line.find(")")]
+                class_name = line[5:line.find("(")]
+            else:
+                parent = None
+                class_name = line[5:]
+            return 'class', class_name,parent
+        elif line[0:3] == 'for':
+            iterators = line[line.find("for")+3:line.find("in")]
+            iterators = iterators.replace(" ",'')
+            iterators = iterators.split(',')
+            arguments = None
+            if '(' and ')' in line:
+                arguments = line[line.find("(")+1:line.find(")")]
+                arguments = self._split_by_operators(arguments)
+                if arguments[0] == '':
+                    arguments = None
+            return 'for', iterators,arguments
+        elif line[0:3] == 'def':
+            name = line[line.find("def")+3:line.find("(")]
+            return 'def',name,None
+        elif '('in line and ')' in line:
+            name =line[:line.find("(")]
+            arguments = line[line.find("(")+1:line.find(")")]
+            arguments = self._split_by_operators(arguments)
+            if arguments[0] == '':
+                arguments = None
+            return 'func',name,arguments
+        return None,None,None
+    
+    def get_data(self,lines):
+        data = {}
+        for i,line in enumerate(lines):
+            row = {}
+            row['original_text'] = line
+            row['level_num'] = self._get_level(line)
+            row['kind'] = None
+            line = line.replace('    ','')
+            line = line.replace('\n','')
+            row['line_text'] = line
+            assign_flag,ass_res = self._look_for_assign(line)
+            if assign_flag:
+                row["resoult"] = ass_res[0]
+                line = ass_res[1]
+            kind,name,arguments = self._get_function(line)
+            row['kind'] = kind
+            row['name'] = name
+            if kind is None:
+                arguments = self._split_by_operators(line)
+            row['arguments'] = arguments
+            data[i] = row
+            #print (split_by_operators(line), data['original_text'])
+            #split_by_operators(line)
+        return data
+    
     def build_tree(path_to_script:str):
         if not os.path.exists(path_to_script):
             return False,"Path doesn not exist"
